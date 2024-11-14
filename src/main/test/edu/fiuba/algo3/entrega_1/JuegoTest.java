9b0d9686e119100e28705c1e15ee18659909244e
Martin Sotelo <lmsotelo@fi.uba.ar>
2024-11-14 01:10:46 -0300

    test04Y05Creados

--- src/main/test/edu/fiuba/algo3/entrega_1/JuegoTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/JuegoTest.java
@@ -43,5 +43,38 @@ public class JuegoTest {
 
     }
 
+    @Test
+    public void test04ValorCorrespondiente(){
+        Jugador jugador = new Jugador();
+        Puntuacion puntacionJugada = new Puntuacion (); //
+        //crear mano Alta
+        List <Card> cartas = new ArrayList<>();
+        Mano mano = new Mano();
+        for (int i = 0; i < 8 ; i ++){
+            mano.recibirCard(cartas[i]);
+        }
+        //jugadaJugador = mano.generarJugada(cartas);
+        jugadaJugador = jugador.realizarJugada(mano);
+        int valor = puntuacionJugada.obtenerValor(jugadaJugador);
+
+        assertEquals(5, valor);
+    }
+    @Test
+    public void test05VerificiarOrdenPuntuacionCartas(){
+        Puntuacion puntacionJugada = new Puntuacion (); //
+        Jugada jugadaJugador = new Jugada();//
+        Combinacion combinacion = new Combinacion();
+        //crear mano Alta y otra mano para comparar, habria que cambiar el valor de mano y despues compararlo en el assert.
+        List <Card> cartas = new ArrayList<>();
+        Mano mano = new Mano();
+        for (int i = 0; i < 8 ; i ++){
+            mano.recibirCard(cartas[i]);
+        }
+        //jugadaJugador = mano.generarJugada(cartas);
+        jugadaJugador = jugador.realizarJugada(mano);
+        combinacion.verificarJugada(jugadaJugador); //dentro de este metodo se deberia llamar a categoria o especial y ver si es una jugada especial.
+        // sino crear 5 instancias de mano y hacer el assert.
+    }
+
 
 }
