a58835af0f4cc351871f1c610492a44117225221
Martin Sotelo <lmsotelo@fi.uba.ar>
2024-11-22 17:58:02 -0300

    correccion errores

---
+++ src/main/java/edu/fiuba/algo3/modelo/MazoPrueba.java
@@ -0,0 +1,15 @@
+package edu.fiuba.algo3.modelo;
+import java.util.List;
+
+
+public class MazoPrueba {
+    private List<Carta> mazo;
+
+    public List<Carta> getMazo() {
+        return mazo;
+    }
+
+    public void setMazo(List<Carta> mazo) {
+        this.mazo = mazo;
+    }
+}
