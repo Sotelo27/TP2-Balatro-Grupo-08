28905417625a16d244dfd6b22ad139b6e5ad6211
Martin Sotelo <lmsotelo@fi.uba.ar>
2024-11-22 18:06:35 -0300

    correccion de rutas Absolutas

--- src/main/java/edu/fiuba/algo3/modelo/JsonReader.java
+++ src/main/java/edu/fiuba/algo3/modelo/JsonReader.java
@@ -10,17 +10,18 @@ import java.util.List;
 public class JsonReader {
 
     public List<Carta> readMazo() throws IOException {
-        // Creamos un ObjectMapper para leer el archivo JSON
+        // Usamos la ruta relativa para acceder al archivo
+        File file = new File(getClass().getClassLoader().getResource("json/mazo.json").getFile());
+
         ObjectMapper objectMapper = new ObjectMapper();
-        // Leemos el archivo JSON y lo deserializamos en un objeto Mazo
-        File file = new File("C:\\Users\\sotel\\OneDrive\\Escritorio\\Balatro\\TP2-Balatro-Grupo-08\\src\\test\\resources\\json\\mazo.json");  // Asumiendo que el archivo se llama "mazo.json"
         MazoPrueba mazo = objectMapper.readValue(file, MazoPrueba.class);
 
-        // Imprimimos las cartas para verificar que se hayan leído correctamente
-        mazo.getMazo().forEach(carta -> System.out.println(carta.getPuntos() + " de " + carta.getPalo()));
+        // Imprimir las cartas para verificar la lectura
+        mazo.getMazo().forEach(carta -> System.out.println(carta.getNombre() + " de " + carta.getPalo()));
 
         return mazo.getMazo();
     }
 }
+
 
 
