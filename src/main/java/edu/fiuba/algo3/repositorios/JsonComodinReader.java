cc8b87d12d5fadbefa19d2643c2ce097bc8742ca
Martin Sotelo <lmsotelo@fi.uba.ar>
2024-11-23 20:29:26 -0300

    lectura de balatro.

--- src/main/java/edu/fiuba/algo3/repositorios/JsonComodinReader.java
+++ src/main/java/edu/fiuba/algo3/repositorios/JsonComodinReader.java
@@ -1,6 +1,5 @@
 package edu.fiuba.algo3.repositorios;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.fiuba.algo3.modelo.*;
@@ -10,55 +9,52 @@ import java.io.IOException;
 import java.util.*;
 
 public class JsonComodinReader {
-    List <CategoriaComodin> categorias;
+    private final String PATH = "json/comodines.json";
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final List<CategoriaComodin> categorias = new ArrayList<>();
 
-    public JsonComodinReader() {categorias = new ArrayList <>();}
+    public JsonComodinReader() {}
+
+    private JsonNode readJsonNode() throws IOException {
+        File file = new File(getClass().getClassLoader().getResource(PATH).getFile());
+        return mapper.readTree(file);
+    }
+
+    public CategoriaComodin readCategoria(String categoryName, Class<? extends CategoriaComodin> clase) throws IOException {
+        JsonNode root = readJsonNode();
+        JsonNode categoryNode = root.get(categoryName);
+        CategoriaComodin categoria = mapper.treeToValue(categoryNode, clase);
+        categorias.add(categoria);
+        return categoria;
+    }
 
     public MazoCombinacion readCombinaciones() throws IOException {
-        File file = new File(getClass().getClassLoader().getResource("json/comodines.json").getFile());
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(file);
+        JsonNode root = readJsonNode();
         JsonNode mixNode = root.get("Combinación");
         return mapper.convertValue(mixNode, MazoCombinacion.class);
     }
 
     public AlPuntaje readCategoriaAlPuntaje() throws IOException {
-        File file = new File(getClass().getClassLoader().getResource("json/comodines.json").getFile());
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(file);
-        JsonNode puntajeNode = root.get("Al Puntaje");
-        categorias.add(mapper.treeToValue(puntajeNode, AlPuntaje.class));
-        return mapper.convertValue(puntajeNode,AlPuntaje.class);
-    }
-    public BonusPorDescarte readCategoriaDescarte() throws IOException {
-        File file = new File(getClass().getClassLoader().getResource("json/comodines.json").getFile());
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(file);
-        JsonNode mixNode = root.get("Bonus por Descarte");
-        categorias.add(mapper.treeToValue(mixNode, BonusPorDescarte.class));
-        return mapper.convertValue(mixNode, BonusPorDescarte.class);
-    }
-    public ComodinAleatorio readCategoriaAleatorio() throws IOException {
-        File file = new File(getClass().getClassLoader().getResource("json/comodines.json").getFile());
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(file);
-        JsonNode mixNode = root.get("Chance de activarse aleatoriamente");
-        categorias.add(mapper.treeToValue(mixNode, ComodinAleatorio.class));
-        return mapper.convertValue(mixNode, ComodinAleatorio.class);
-    }
-    public BonusPorManoJugada readCategoriaManoJugada() throws IOException {
-        File file = new File(getClass().getClassLoader().getResource("json/comodines.json").getFile());
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(file);
-        JsonNode mixNode = root.get("Bonus por Mano Jugada");
-        categorias.add(mapper.treeToValue(mixNode, BonusPorManoJugada.class));
-        return mapper.convertValue(mixNode, BonusPorManoJugada.class);
+        return (AlPuntaje) readCategoria("Al Puntaje", AlPuntaje.class);
     }
 
-    public List<CategoriaComodin> getCategorias() {
+    public BonusPorDescarte readCategoriaDescarte() throws IOException {
+        return (BonusPorDescarte) readCategoria("Bonus por Descarte", BonusPorDescarte.class);
+    }
+
+    public ComodinAleatorio readCategoriaAleatorio() throws IOException {
+        return (ComodinAleatorio) readCategoria("Chance de activarse aleatoriamente", ComodinAleatorio.class);
+    }
+
+    public BonusPorManoJugada readCategoriaManoJugada() throws IOException {
+        return (BonusPorManoJugada) readCategoria("Bonus por Mano Jugada", BonusPorManoJugada.class);
+    }
+
+    public List<CategoriaComodin> readCategorias() {
         return categorias;
     }
 }
+
 
 
 
