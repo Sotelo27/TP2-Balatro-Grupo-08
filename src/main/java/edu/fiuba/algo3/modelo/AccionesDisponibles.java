6bb2310a63376997f2430addfd3131e378148e4d
Joaquin <hernandez.joaquin346@gmail.com>
2024-11-23 17:57:11 -0300

    clase que guarda las limitaciones de la ronda

---
+++ src/main/java/edu/fiuba/algo3/modelo/AccionesDisponibles.java
@@ -0,0 +1,26 @@
+class AccionesDisponibles{
+	private int manosDisponibles;
+	private int descartesDisponibles;
+
+	public AccionesDisponibles(int manos, int descartes){
+		this.manosDisponibles = manos;
+		this.descartesDisponibles = descartes;
+	}
+
+	public boolean superaLimiteDeDescartes(int descartes){
+		return this.descartesDisponibles > descartes;
+	}
+
+	public boolean quedanManosDisponibles(int manos){
+		return this.manosDisponibles > 0;
+	}
+
+	public void reducirDescartesEn(int descartes){
+		this.descartesDisponibles -= descartes;
+	}
+
+	public void reducirManosDisponibles(){
+		this.manosDisponibles -= 1;
+	}
+	
+}
