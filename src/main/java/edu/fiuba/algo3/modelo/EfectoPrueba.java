bb6e723ef5c3fbafea06cd6f5c9bb8ff06e858ea
Martin Sotelo <lmsotelo@fi.uba.ar>
2024-11-22 21:48:56 -0300

    lectura de json de tarots

--- src/main/java/edu/fiuba/algo3/modelo/EfectoPrueba.java
+++ src/main/java/edu/fiuba/algo3/modelo/EfectoPrueba.java
@@ -4,8 +4,28 @@ public class EfectoPrueba {
     private int puntos;
     private int multiplicador;
 
+    // Getters y setters
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getMultiplicador() {
+        return multiplicador;
+    }
+
+    public void setMultiplicador(int multiplicador) {
+        this.multiplicador = multiplicador;
+    }
+
     @Override
     public String toString() {
-        return super.toString();
+        return "EfectoPrueba{" +
+                "puntos=" + puntos +
+                ", multiplicador=" + multiplicador +
+                '}';
     }
 }
--- src/main/java/edu/fiuba/algo3/repositorios/JsonTarotReader.java
+++ src/main/java/edu/fiuba/algo3/repositorios/JsonTarotReader.java
@@ -1,28 +1,35 @@
 package edu.fiuba.algo3.repositorios;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.fiuba.algo3.modelo.EfectoPrueba;
-import edu.fiuba.algo3.modelo.MazoTarots;
-
+import edu.fiuba.algo3.modelo.Tarot;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.util.List;
 
 public class JsonTarotReader {
-    public void readTarots() throws IOException {
-        // Usamos la ruta relativa para acceder al archivo
+    public List<Tarot> readTarots() throws IOException {
+        // Ruta al archivo
         File file = new File(getClass().getClassLoader().getResource("json/tarots.json").getFile());
 
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(file);
 
-        JsonNode tarots = root.get("tarots");
-        if(tarots.isArray()){
-            for(JsonNode tarot : tarots){
-                System.out.println(tarot.toString());
+        JsonNode tarotsNode = root.get("tarots");
+        if (tarotsNode.isArray()) {
+            // Mapea el JSON directamente a una lista de objetos Tarot
+            List<Tarot> tarots = mapper.convertValue(tarotsNode, new TypeReference<List<Tarot>>() {});
+
+            // Imprime cada objeto Tarot para verificar
+            for (Tarot tarot : tarots) {
+                System.out.println(tarot.getNombre()); // Aquí se imprime cada tarot
             }
+            return tarots;
         }
+
+        throw new IOException("El nodo 'tarots' no es un arreglo.");
     }
 }
+
