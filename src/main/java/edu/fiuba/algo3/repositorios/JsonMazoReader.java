fca9f47b3599786982294c8794b808fef13c4fba
Martin Sotelo <lmsotelo@fi.uba.ar>
2024-11-23 12:52:18 -0300

    correccion de setters

---
+++ src/main/java/edu/fiuba/algo3/repositorios/JsonMazoReader.java
@@ -0,0 +1,26 @@
+package edu.fiuba.algo3.repositorios;
+import edu.fiuba.algo3.modelo.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonMazoReader {
+
+    public MazoPrueba readMazo() throws IOException {
+        // Usamos la ruta relativa para acceder al archivo
+
+        File file = new File(getClass().getClassLoader().getResource("json/mazo.json").getFile());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        MazoPrueba mazo = objectMapper.readValue(file, MazoPrueba.class);
+
+        // Imprimir las cartas para verificar la lectura
+       // mazo.getMazo().forEach(carta -> System.out.println(carta.getNumero()));
+
+        return mazo;
+    }
+}
