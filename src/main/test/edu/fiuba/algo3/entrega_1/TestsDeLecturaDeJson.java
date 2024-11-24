341f4cdf777cc8117ccaccd11acdf8874fdd73d5
Martin Sotelo <lmsotelo@fi.uba.ar>
2024-11-24 14:56:09 -0300

    Test De Lectura De Json

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/TestsDeLecturaDeJson.java
@@ -0,0 +1,35 @@
+package edu.fiuba.algo3.entrega_1;
+import edu.fiuba.algo3.repositorios.*;
+import edu.fiuba.algo3.modelo.*;
+import org.junit.Test;
+import java.io.IOException;
+import java.util.List;
+
+public class TestsDeLecturaDeJson {
+    @Test
+    public void verificarJsonReader() throws IOException {
+        JsonMazoReader jsonReader = new JsonMazoReader();
+        jsonReader.readMazo();
+    }
+
+    @Test
+    public void verificarTarotReader() throws IOException {
+        JsonTarotReader jsonReader = new JsonTarotReader();
+        jsonReader.readTarots();
+    }
+
+
+    @Test
+    public void verificaComodinReader() throws IOException {
+        JsonComodinReader jsonReader = new JsonComodinReader();
+        List<Comodin> mazo = jsonReader.readComodines();
+    }
+    @Test
+    public void verificarBalatroReader() throws IOException {
+        JsonBalatroReader jsonReader = new JsonBalatroReader();
+        Balatro juego = jsonReader.readBalatro();
+        List<Ronda> rondas = juego.getRondas();
+    }
+}
+
+
