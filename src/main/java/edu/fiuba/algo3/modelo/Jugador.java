c3083944268c31f4e30cfafcad598c39382c0141
Joaquin <hernandez.joaquin346@gmail.com>
2024-11-24 00:51:59 -0300

    se implementa jugador con sus metodos

--- src/main/java/edu/fiuba/algo3/modelo/Jugador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Jugador.java
@@ -1,11 +1,33 @@
 package edu.fiuba.algo3.modelo;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Jugador {
-    private String name;
+    private String nombre;
+    private Ficha fichas;
+    private Mano mano
+    private List<Comodin> comodinesActivos;
+    private List<Tarot> tarotsActivos;
 
+    public Jugador(String nombre, Mazo mazo){
+        this.nombre = nombre;
+        this.mazo = mazo;
+        this.mano = new Mano(mazo);
+    }
+    public void seleccionarCarta(Carta cartasSeleccionada){
+        this.mazo.seleccionarCarta(cartasSeleccionada);
+    }
 
-
-
-
-
+    public void realizarJugada(Ronda ronda){
+        this.mano.realizarJugada(ronda, tarotsActivos, comodinesActivos);
+    }
+    public void realizarDescarte(Ronda ronda){
+        this.mano.realizarDescarte(Ronda ronda);
+    }
+    public void activarTarot(Tarot tarot){
+        this.tarotsActivos.add(tarot);
+    }
+    public void activarComodin(Comodin comodin){
+        this.comodinesActivos.add(comodin);
+    }
 }
