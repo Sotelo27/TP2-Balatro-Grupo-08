3ab58b56bde64c6070f94fe251a4488c55e9b7b0
Joaquin <hernandez.joaquin346@gmail.com>
2024-11-24 01:27:15 -0300

    eliminados objetos duplicados

--- src/main/java/edu/fiuba/algo3/modelo/Straight.java
+++
@@ -1,9 +0,0 @@
-package edu.fiuba.algo3.modelo;
-
-import java.util.*;
-
-public class Straight extends CombinacionDePoker{
-    public Straight(List<CartaDePoker> cartas) {
-        super(cartas);
-    }
-}
