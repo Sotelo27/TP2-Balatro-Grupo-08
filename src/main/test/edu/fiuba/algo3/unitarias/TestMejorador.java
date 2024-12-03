fdf7926d50d89dfb7466bd3540951b024ed44c6f
Joaquin H <hernandez.joaquin346@gmail.com>
2024-12-02 21:57:28 -0300

    Se agregan comentarios de guia para la creacion de pruebas

--- src/main/test/edu/fiuba/algo3/unitarias/TestMejorador.java
+++ src/main/test/edu/fiuba/algo3/unitarias/TestMejorador.java
@@ -1,4 +1,10 @@
 package edu.fiuba.algo3.unitarias;
 
 public class TestMejorador {
+
+    /*
+    * Validar que se el el aplicar mejora sobre cualquier mejorable genera el puntaje deseado
+    * Validar que se pueden modificar los mejorables de todas las meneras que permite el json de comodines y tarots
+    * Validar que las restricciones se cumplen y aplican para cada tipo de restriccion
+    * */
 }
--- src/main/test/edu/fiuba/algo3/unitarias/TestRonda.java
+++ src/main/test/edu/fiuba/algo3/unitarias/TestRonda.java
@@ -2,4 +2,12 @@ package edu.fiuba.algo3.unitarias;
 
 import junit.framework.TestCase;
 public class TestRonda {
+
+/*
+* validar que los puntajes se muestran correctamente luego de cada jugada
+* Validar que los puntajes se calculan correctamente luego de cada jugada
+* Validar que los limites de las Rondas son utilizados y disparados para las derrotas
+* Validar que las rondas pueden ser superadas luego de alcanzar el puntaje obtenido
+* Validar que la tienda de cada Ronda puede ser utilizada correctamente
+* */
 }
