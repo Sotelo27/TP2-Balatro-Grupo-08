30d58f73f5e2d1708db6a2c0af1ad700fb8c7fe9
Joaquin H <hernandez.joaquin346@gmail.com>
2024-12-02 21:25:15 -0300

    Se aniaden tests para hacer tdd al mazo

--- src/main/test/edu/fiuba/algo3/unitarias/TestMazo.java
+++ src/main/test/edu/fiuba/algo3/unitarias/TestMazo.java
@@ -1,10 +1,51 @@
 package edu.fiuba.algo3.unitarias;
+import edu.fiuba.algo3.modelo.CartaDePoker;
+import edu.fiuba.algo3.modelo.Mano;
 import edu.fiuba.algo3.modelo.Mazo;
+import edu.fiuba.algo3.modelo.Ronda;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
 
 public class TestMazo {
     @Test
-    public void test0SeReincorporanLasCartasUtilizadasLuegoDeQuedarseSinCartasElMazo(){
-
+    public void test01SeReincorporanLasCartasUtilizadasLuegoDeUnDescarte(){
+        Ronda rondaMock = mock(Ronda.class);
+        Mazo mazo = new Mazo();
+        List<CartaDePoker> cartasParaElMazo = new ArrayList<>();
+        for(int i= 0 ; i < 8; i++){
+            cartasParaElMazo.add(new CartaDePoker());
+        }
+        mazo.setMazo(cartasParaElMazo);
+        Mano mano = new Mano(mazo);
+        for(int i = 0 ; i < 4; i++){
+            mano.seleccionarCarta((cartasParaElMazo.get(1)));
+        }
+        mano.realizarDescarte(rondaMock, new ArrayList<>(), mazo);
+        mano.recargarManoConMazo(mazo);
+        List<String> cartasEnMano = mano.getCartasEnMano();
+        assert cartasEnMano.size() == 8;
     }
+    @Test
+    public void test02SeReincorporanLasCartasUtilizadasLuegoDeUnaJugada(){
+        Ronda rondaMock = mock(Ronda.class);
+        Mazo mazo = new Mazo();
+        List<CartaDePoker> cartasParaElMazo = new ArrayList<>();
+        for(int i= 0 ; i < 8; i++){
+            cartasParaElMazo.add(new CartaDePoker());
+        }
+        mazo.setMazo(cartasParaElMazo);
+        Mano mano = new Mano(mazo);
+        for(int i = 0 ; i < 4; i++){
+            mano.seleccionarCarta((cartasParaElMazo.get(1)));
+        }
+        mano.realizarJugada(rondaMock, new ArrayList<>(), new ArrayList<>(), mazo);
+        mano.recargarManoConMazo(mazo);
+        List<String> cartasEnMano = mano.getCartasEnMano();
+        assert cartasEnMano.size() == 8;
+    }
+
 }
