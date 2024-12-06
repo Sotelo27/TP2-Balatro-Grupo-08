84fdfc75bd11b24a32143468b93852c7734400b9
Joaquin H <hernandez.joaquin346@gmail.com>
2024-12-06 17:47:22 -0300

    Se puede pasar correctamente de escena Title a Shop

--- src/main/java/edu/fiuba/algo3/controllers/PlayerSceneController.java
+++ src/main/java/edu/fiuba/algo3/controllers/PlayerSceneController.java
@@ -5,6 +5,7 @@ import edu.fiuba.algo3.repositorios.LectorDeJSON;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,8 +14,10 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PlayerSceneController {
+public class PlayerSceneController implements Initializable {
     private BalatroAlgo3 modelo;
 
     @FXML
@@ -31,20 +34,41 @@ public class PlayerSceneController {
         switcher = new SceneController();
     }
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+
     @FXML
     void saveName(ActionEvent event) throws IOException {
         this.userName = usernameField.getText();
         System.out.println(this.userName);
         this.modelo = new BalatroAlgo3(this.userName, new LectorDeJSON());
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/shopScene.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    void saveNameAndStartGame(ActionEvent event) throws IOException {
+        this.userName = usernameField.getText();
+        System.out.println(this.userName);
+        this.modelo = new BalatroAlgo3(this.userName, new LectorDeJSON());
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/shopScene.fxml"));
+        Parent gameShopRoot = loader.load();
+        ShopSceneController controller = loader.getController();
+        controller.setModelo(this.modelo);
+
+        btnNameSaver.getScene().setRoot(gameShopRoot);
+    }
 
     @FXML
     void backToTitle(ActionEvent event) throws IOException {
         switcher.switchToMainScene(event);
     }
+
 }
