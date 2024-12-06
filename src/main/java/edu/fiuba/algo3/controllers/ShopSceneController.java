ba3c1f3f0f29265cdb58968033b199054d9c4c7d
Bryan <bserrantes@fi.uba.ar>
2024-12-05 23:03:39 -0300

    add imagenesClickeables

--- src/main/java/edu/fiuba/algo3/controllers/ShopSceneController.java
+++ src/main/java/edu/fiuba/algo3/controllers/ShopSceneController.java
@@ -2,6 +2,7 @@ package edu.fiuba.algo3.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class ShopSceneController {
 
@@ -20,5 +21,28 @@ public class ShopSceneController {
     @FXML
     private ImageView thrdCardOffert;
 
+    @FXML
+    void selectFirstCard(MouseEvent event) {
+        System.out.println("firstCardOffert selected");
+    }
 
+    @FXML
+    void selectFifthCard(MouseEvent event) {
+        System.out.println("fifthCardOffert selected");
+    }
+
+    @FXML
+    void selectFourthCard(MouseEvent event) {
+        System.out.println("fourthCardOffert selected");
+    }
+
+    @FXML
+    void selectScndCard(MouseEvent event) {
+        System.out.println("scndCardOffert selected");
+    }
+
+    @FXML
+    void selectThirdCard(MouseEvent event) {
+        System.out.println("thirdCardOffert selected");
+    }
 }
