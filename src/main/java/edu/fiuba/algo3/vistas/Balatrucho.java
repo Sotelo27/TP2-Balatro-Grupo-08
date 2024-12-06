4b5a0f304cade96f39ad33c4b5b263ed0749b99d
Joaquin H <hernandez.joaquin346@gmail.com>
2024-12-06 17:18:02 -0300

    renombrado app a Balatrucho y seteado como init

--- src/main/java/edu/fiuba/algo3/vistas/Balatrucho.java
+++ src/main/java/edu/fiuba/algo3/vistas/Balatrucho.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.vistas;
 import edu.fiuba.algo3.SystemInfo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -15,21 +16,29 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class Balatrucho extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        // Load FXML and set controller
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/titleScene.fxml"));
+        Parent root = loader.load();
+
+        // Set the scene
+        Image icon = new Image(getClass().getResourceAsStream("/images/mainIcon.png"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Balatrucho-Fiuba");
         stage.setScene(scene);
+        stage.getIcons().add(icon);
         stage.show();
+
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
