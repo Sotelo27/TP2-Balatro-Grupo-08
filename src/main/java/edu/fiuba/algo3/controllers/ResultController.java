50f4ab9ab08977a475f16fc6487a86339d70c782
Martin Sotelo <lmsotelo@fi.uba.ar>
2024-12-11 20:59:14 -0300

    implementacion de reinicio implementada.

--- src/main/java/edu/fiuba/algo3/controllers/ResultController.java
+++ src/main/java/edu/fiuba/algo3/controllers/ResultController.java
@@ -1,14 +1,23 @@
 package edu.fiuba.algo3.controllers;
 
 import edu.fiuba.algo3.modelo.BalatroAlgo3;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import java.io.IOException;
+
 public class ResultController {
+    private SceneController switcher;
+    public ResultController(){
+        this.switcher = new SceneController();
+    }
+
     @FXML
-    public void restartGame() {
+    public void restartGame(ActionEvent event) throws IOException {
         // Lógica para reiniciar el juego
         System.out.println("Reiniciando el juego...");
         // Cambiar a la escena del juego
+        switcher.switchToMainScene(event);
     }
 
     @FXML
@@ -16,6 +25,7 @@ public class ResultController {
         // Lógica para volver al menú principal
         System.out.println("Volviendo al menú principal...");
         // Cambiar a la escena del menú principal
+
     }
 
     public void setModelo(BalatroAlgo3 modelo) {
